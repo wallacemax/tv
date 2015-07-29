6d3de8c10ebfb2522f7e14052fee703eed62e93f
Max Wallace <wallace.max@gmail.com>
2015-07-29 16:49:07 -0400

    now with subplots and labels!

--- python/tv/tv.py
+++ python/tv/tv.py
@@ -5,11 +5,13 @@ import os
 import datetime
 
 import matplotlib
+import matplotlib.pyplot as plt
 
-matplotlib.use("TkAgg")
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+
+from time import strftime
 
 USE_MDS_DATA = True
 try:
@@ -22,7 +24,7 @@ import ctypes
 
 defaultdpi = 100
 radialgraphwidth = 5
-radialgraphratio = .45
+radialgraphratio = 1.2
 timegraphwidth = 5
 timegraphratio = .6
 
@@ -31,7 +33,7 @@ userid = os.getenv('LOGNAME')
 
 include_csv = 0
 
-plotfont = {'family': 'Helvetica', 'size': 12}
+plotfont = {'family': 'san-serif', 'size': 12}
 matplotlib.rc('font', **plotfont)
 
 
@@ -56,11 +58,11 @@ class tvMain:
                          'CPLASMA': [],
                          'WMHD': []}
 
-        self.MDS_data_titles = {'FIT_NE': 'Ne units',
-                                'FIT_PE': 'Pe units',
-                                'FIT_TE:': 'Te units',
-                                'CPLASMA': 'Current units',
-                                'WMHD': 'Energy units'}
+        self.MDS_data_titles = {'FIT_NE': 'Density (Ne)',
+                                'FIT_PE': 'Pressure (Pe)',
+                                'FIT_TE': 'Temperature (Te)',
+                                'CPLASMA': 'Plasma Current',
+                                'WMHD': 'Stored Energy'}
 
         self.update_text.set('Please input shot number for visualization.')
 
@@ -76,7 +78,7 @@ class tvMain:
         btnPNG = tk.Button(text="Print PNG", command=lambda: self.export_graphs(self.txtShotNumber.get(), 'PNG'))
         btnPNG.grid(row=3, column=0)
         self.include_csv = tk.IntVar()
-        self.chkCSV = tk.Checkbutton(text="Save CSV", variable=include_csv)  # , command=self.printCSV)
+        self.chkCSV = tk.Checkbutton(text="Save CSV", variable=include_csv)
         self.chkCSV.grid(row=3, column=0, sticky=tk.E)
 
     def drawGraphs(self):
@@ -85,54 +87,45 @@ class tvMain:
         self.drawTimeGraphs()
 
     def drawRadialGraphs(self, framenumber=0):
+
+        self.framenumber = framenumber
+
         radialFrame = tk.Frame(self.master)
-        radialFrame.highlightthickness = 0
         radialFrame.grid(row=2, column=0)
 
         ne = self.MDS_data['FIT_NE']
         te = self.MDS_data['FIT_TE']
         pe = self.MDS_data['FIT_PE']
 
-        self.figure_1 = Figure(figsize=(radialgraphwidth,
-                                        radialgraphwidth * radialgraphratio),
-                                        dpi=defaultdpi,
-                                        facecolor='white',
-                                        )
-        ax_1 = self.figure_1.add_subplot(111)
-        ax_1.set_xlabel('Radial Profile')
-        ax_1.set_ylabel('eV')
-        self.figure_1.suptitle('Density (Ne)')
-        self.fig_1, = ax_1.plot(ne[framenumber])
+        self.figure_1 = plt.figure(figsize=(radialgraphwidth,
+                                            radialgraphwidth * radialgraphratio),
+                                   dpi=defaultdpi,
+                                   facecolor='white')
+
         self.figure_1.add_axes()
+        self.figure_1.add_subplot(311)
+
+        self.ax1 = plt.subplot(3, 1, 1)
+        self.ax1.plot(ne[framenumber])
+        self.ax1.set_title(self.MDS_data_titles['FIT_NE'])
+        self.ax1.set_ylabel('eV?')
+
+        self.ax2 = plt.subplot(3, 1, 2)
+        self.ax2.plot(te[framenumber])
+        self.ax2.set_title(self.MDS_data_titles['FIT_TE'])
+        self.ax2.set_ylabel('eV?')
+
+        self.ax3 = plt.subplot(3, 1, 3)
+        self.ax3.plot(pe[framenumber])
+        self.ax3.set_title(self.MDS_data_titles['FIT_PE'])
+        self.ax3.set_ylabel('eV?')
+
+        self.ax3.set_xlabel('Radial Profile')
+        self.figure_1.subplots_adjust(hspace=1)
+
         self.canvas_1 = FigureCanvasTkAgg(self.figure_1, master=radialFrame)
         self.canvas_1.show()
         self.canvas_1.get_tk_widget().grid(row=0, column=0)
-
-        self.figure_2 = Figure(figsize=(radialgraphwidth,
-                                        radialgraphwidth * radialgraphratio), dpi=defaultdpi, facecolor='white')
-        ax_2 = self.figure_2.add_subplot(111)
-        ax_2.set_xlabel('Radial Profile')
-        ax_2.set_ylabel('eV')
-        self.figure_2.suptitle('Temperature (Te)')
-        self.fig_2, = ax_2.plot(te[framenumber])
-        self.canvas_2 = FigureCanvasTkAgg(self.figure_2, master=radialFrame)
-        self.canvas_2.show()
-        self.canvas_2.get_tk_widget().grid(row=1, column=0)
-
-        self.figure_3 = Figure(figsize=(radialgraphwidth,
-                                        radialgraphwidth * radialgraphratio), dpi=defaultdpi, facecolor='white')
-        ax_3 = self.figure_3.add_subplot(111)
-        ax_3.set_xlabel('Radial Profile')
-        ax_3.set_ylabel('eV')
-        ax_3.spines['top'].set_visible(False)
-        ax_3.spines['right'].set_visible(False)
-        ax_3.spines['bottom'].set_visible(False)
-        ax_3.spines['left'].set_visible(False)
-        self.figure_3.suptitle('Pressure (Pe)')
-        self.fig_3, = ax_3.plot(pe[framenumber])
-        self.canvas_3 = FigureCanvasTkAgg(self.figure_3, master=radialFrame)
-        self.canvas_3.show()
-        self.canvas_3.get_tk_widget().grid(row=2, column=0)
 
     def drawTimeGraphs(self):
 
@@ -147,7 +140,7 @@ class tvMain:
         ax_a = self.figure_a.add_subplot(111)
         ax_a.set_xlabel('Time (ms)')
         ax_a.set_ylabel('eV ?')
-        self.figure_a.suptitle('Plasma Current')
+        self.figure_a.suptitle(self.MDS_data_titles['CPLASMA'])
         self.fig_a, = ax_a.plot(cplasma)
         self.canvas_a = FigureCanvasTkAgg(self.figure_a, master=timeFrame)
         self.canvas_a.show()
@@ -158,7 +151,7 @@ class tvMain:
         ax_b = self.figure_b.add_subplot(111)
         ax_b.set_xlabel('Time (ms)')
         ax_b.set_ylabel('eV ?')
-        self.figure_b.suptitle('Stored Energy')
+        self.figure_b.suptitle(self.MDS_data_titles['WMHD'])
         self.fig_b, = ax_b.plot(wmhd)
         self.canvas_b = FigureCanvasTkAgg(self.figure_b, master=timeFrame)
         self.canvas_b.show()
@@ -179,9 +172,9 @@ class tvMain:
         self.btnShot = tk.Button(self.entryFrame, image=self.playLogo, command=self.shotnumberInput)
         self.btnShot.pack(side=tk.LEFT)
 
-        lblTime = tk.Label(text='Time', background='white')
+        lblTime = tk.Label(text=strftime('%H:%M:%S'), background='white')
         lblTime.grid(row=0, column=0)
-        lblUserID = tk.Label(text='UserID', background='white')
+        lblUserID = tk.Label(text=userid, background='white')
         lblUserID.grid(row=0, column=0, sticky=tk.E)
 
         self.headerLogo = tk.PhotoImage(file="logo.gif")
@@ -200,23 +193,56 @@ class tvMain:
         timestamp = datetime.datetime.isoformat(
             datetime.datetime.today()).split('.')[0].replace('-', '').replace(':', '')
 
-        updatestring = 'Images for {} saved in {} format'.format(shotid, file_type)
+        updatestring = 'Images for {} saved in {} format'.format(shotid,
+                            file_type + ' and CSV' if self.include_csv.get() else file_type)
 
         if not os.path.exists(shotnumber):
             os.makedirs(shotnumber)
 
-        for graphName in {'figure_a', 'figure_b', 'figure_c', 'figure_1', 'figure_2'}:
+        for graphName in {'figure_a', 'figure_b', 'figure_1'}:
             graph = getattr(self, graphName)
-            fileName = '{}/{}_{}_{}.{}'.format(shotnumber, shotnumber, graph._suptitle._text, timestamp,
+
+            try:
+                graphTitle = graph._suptitle._text
+            except AttributeError:
+                graphTitle = 'ThomsonData'
+
+            fileName = '{}/{}_{}_{}.{}'.format(shotnumber, shotnumber, graphTitle, timestamp,
                                                file_type).replace(' ', '')
             graph.savefig(fileName, dpi=defaultdpi, format=file_type, bbox_inches='tight', frameon=None)
 
-            if self.include_csv:
-                line = graph.gca().get_lines()[0].get_xydata()
-                np.savetxt(fileName.replace(file_type, 'csv'), line, delimiter=',')
-                updatestring += ' and CSV'
+            if self.include_csv.get():
+                self.export_csv_data(fileName, file_type, graph, graphTitle)
 
         self.update_text.set(updatestring)
+
+    def export_csv_data(self, fileName, file_type, graph, graphTitle):
+        if graphTitle == 'ThomsonData':
+            self.export_thomson_csv_data(fileName, file_type)
+        else:
+            self.export_time_csv_data(fileName, file_type, graph)
+
+    def export_time_csv_data(self, fileName, file_type, graph):
+        line = graph.gca().get_lines()[0].get_xydata()
+        np.savetxt(fileName.replace(file_type, 'csv'), line, delimiter=',')
+
+    def export_thomson_csv_data(self, fileName, file_type):
+        for i in range(1, 3):
+            sub = getattr(self, 'ax' + str(i))
+            datacaption = sub.get_title()
+            graphdata = self.get_graph_data(datacaption, self.framenumber)
+
+            np.savetxt(fileName.replace('ThomsonData', datacaption)
+                       .replace(file_type, 'csv'), graphdata, delimiter=',')
+
+    def get_graph_data(self, datacaption, framenumber):
+        key = ''
+        for item in self.MDS_data_titles.items():
+            if item[1] == datacaption:
+                key = item[0]
+                break
+
+        return self.MDS_data[key][framenumber]
 
     def shotnumberInput(self):
         shotnumber = self.txtShotNumber.get()
@@ -268,10 +294,10 @@ class tvMain:
 def main():
     root = tk.Tk()
     root.wm_title("Thomson Visualization")
-    root.geometry('{}x{}'.format(1200, 1200))
+    root.geometry('{}x{}'.format(1024, 768))
     root["bg"] = "white"
-    root.grid_columnconfigure(0, uniform="also", minsize=600)
-    root.grid_columnconfigure(1, uniform="also", minsize=600)
+    root.grid_columnconfigure(0, uniform="also", minsize=512)
+    root.grid_columnconfigure(1, uniform="also")
     root["bd"] = 0
     app = tvMain(root)
 
